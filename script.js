b829b322d91a06dde2aaa2c63efb645a4788884c
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-11-02 21:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your code here
 
 function change() {
-	let app = document.getElementById("app");
-	app.style.backgroundColor = "black";
+	const appDiv = document.getElementById("app");
+  const button = document.getElementById("swap");
+
+  // Toggle between 'day' and 'night' classes on the main div
+  appDiv.classList.toggle("day");
+  appDiv.classList.toggle("night");
+
+  // Toggle between 'button_day' and 'button_night' classes on the button
+  button.classList.toggle("button_day");
+  button.classList.toggle("button_night");
 }
